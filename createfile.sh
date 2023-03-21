9c52fcc67311c8f318cb2b2eaeec7148912e1116
TruphenaKwamboka <trusoro32@gmail.com>
2023-03-21 10:56:06 -0700

    _putchar

--- createfile.sh
+++ createfile.sh
@@ -3,4 +3,4 @@ clear
 
 read -p "Enter name of file: " filename
 
-vim 0x01-variables_if_else_while/$filename && chmod +x 0x01-variables_if_else_while/$filename
+vim 0x02-functions_nested_loops/$filename && chmod +x 0x02-functions_nested_loops/$filename
