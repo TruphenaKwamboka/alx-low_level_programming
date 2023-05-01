a8f87a7e852e416a8883aaa40edc722dcfbac672
TruphenaKwamboka <trusoro32@gmail.com>
2023-05-01 02:37:11 -0700

    task update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list at a specified position
+ * insert_nodeint_at_index - inserts a new node in a
+ * linked list at a specified position
  * @head: pointer to the first node of the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
